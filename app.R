df7902b904aa32c3cf4e27c7f8f41af9216e6ade
chain322 <chain322@gmail.com>
2023-12-10 15:59:14 -0800

    Structured the app and fixed some errors in the project file

--- app.R
+++ app.R
@@ -18,25 +18,72 @@ intro_view <- fluidPage(
 
 analysis_view <- fluidPage(
 #This is where the 3 interactive datasets should go
+  titlePanel("Wealth Gap Analysis"),
+  sidebarLayout(
+    sidebarPanel = 
+      selectInput(
+        inputId = "graph_choice",
+        label = "Select Dataset",
+        choices = list("Median income VS. CPP", "Median Income Affected by CPP", "Racial disparity in income"),
+        htmlOutput(outputId = "data_group"),
+        htmlOutput(outputId = "data_intro")
+      ),
+    mainPanel(
+      plotlyOutput(outputId = "data")
+    )
+  ),
   
-  
+  titlePanel("What can we assume from this data?"),
+  br(),
+  htmlOutput(outputId = "data_assump"),
 )
+  
+
 
 ui <- navbarPage( #this creates the tabs at top and the title in corner
   
   "The Growing Wealth Gap",
   tabPanel("Introduction", intro_view),
   tabPanel("Analysis", analysis_view)
-  
-  
 )
+
 
 
 # server interface
 server <- function(input, output) {
   
+  # need to write function in project.R to pull one of 3 graphs: med income line, CPP affected income, or racial income based on input 
+  # from choices above. Need to make the three graphs in project.R as well.  
   
+  output$data_group <- renderUI({
+    
+    # create a for loop that displays a more professional title depending on what is selected from the dropdown.
+    
+    # "US Median Income Growth and Consumer Purchasing Power Decline", "How Consumer Purchasing Power affects our income", 
+    # "The Growing Gap: Racial Systematic Oppression Observed Through Income Trends"
+  })
   
+  output$data <- renderPlotly({
+    
+    # assign p with the value of the function that puts out one of the 3 graphs depending on what the user selects from the dropdown
+    
+    # p <- ggplotly(p, tooltip = "text")
+    # return(p)
+    # plot(p)
+    
+  })
+  
+  output$data_intro <- renderUI({
+    
+    # Create a for loop that displays the introduction of the data that has been selected. 
+    
+  })
+  
+  output$data_assump <- renderUI({
+    
+    # create a for loop that displays the conclusion of the data that has been selected.
+    
+  })
   
   
   
